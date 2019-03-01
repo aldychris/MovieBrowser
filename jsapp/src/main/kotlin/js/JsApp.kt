924794d7eb63930a057560276fb8180e9f1b7972
AldyChris <aldychris@gmail.com>
2019-03-01 17:55:07 +0800

    Rename to index and add css file

--- jsapp/src/main/kotlin/js/JsApp.kt
+++ jsapp/src/main/kotlin/js/JsApp.kt
@@ -1,11 +1,14 @@
-package main.kotlin.js
+package js
+
+import data.api.MovieApiService
 
 fun main(arguments: Array<String>) {
 
     val message = "Hello JavaScript!"
     println(message)
 
-
+    val api = MovieApiService("https://www.omdbapi.com", "")
+    println(api)
 }
 
 
